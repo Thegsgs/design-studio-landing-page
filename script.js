59442e841b12ce710f987bc832ed12bbad332251
Simon <simonshrb@gmail.com>
2021-08-19 18:15:41 +0300

    feat: dining images changing script

--- script.js
+++ script.js
@@ -1,36 +1,47 @@
-let rightButton = document.querySelector('.project__arrow-container_right');
-let leftButton = document.querySelector('.project__arrow-container_left');
-
-
+let rightButton = document.getElementById('dining-right');
+let leftButton = document.getElementById('dining-left');
+let currentImage = document.getElementById('dining-image');
 let diningArray = new Array();
 
 diningArray[0] = new Image();
-diningArray[0].src = './images/dining2.jpg';
+diningArray[0].src = './images/dining1.jpg';
 
 diningArray[1] = new Image();
-diningArray[1].src = './images/dining3.jpg';
+diningArray[1].src = './images/dining2.jpg';
 
 diningArray[2] = new Image();
-diningArray[2].src = './images/dining4.jpg';
+diningArray[2].src = './images/dining3.jpg';
 
 diningArray[3] = new Image();
-diningArray[3].src = './images/dining5.jpg';
+diningArray[3].src = './images/dining4.jpg';
 
-function nextImage(array) {
+diningArray[4] = new Image();
+diningArray[4].src = ' ./images/dining5.jpg';
 
-  let image = document.querySelector('.project__image');
+imagePosition = 0;
+console.log(imagePosition);
 
-  for (let i = 0; i < array.length; i++) {
+function nextImage() {
+  if (imagePosition === diningArray.length - 1) {
+    imagePosition = 0;
+    currentImage.src = diningArray[imagePosition].src;
+    console.log(imagePosition);
+  } else {
+    imagePosition += 1;
+    currentImage.src = diningArray[imagePosition].src;
+    console.log(imagePosition);
+  }
+}
 
-    if (imgArray[i].src == img.src) // << check this
-    {
-      if (i === array.length) {
-        document.getElementById(element).src = imgArray[0].src;
-        break;
-      }
-      document.getElementById(element).src = imgArray[i + 1].src;
-      break;
-    }
+function previousImage() {
+  if (imagePosition === 0) {
+    imagePosition = diningArray.length - 1;
+    currentImage.src = diningArray[imagePosition].src;
+    console.log(imagePosition);
+  } else {
+    imagePosition -= 1;
+    currentImage.src = diningArray[imagePosition].src;
+    console.log(imagePosition);
   }
 }
 
